6965a3d6e0b119fc9da8cf085599571d09998ca9
Modularz <79064796+designify123@users.noreply.github.com>
2022-01-12 08:44:46 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,16 @@
 local altstotal = getgenv().#alts
 
+function chat(msg)
+  game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest(msg, "All")
+end)
+
+function begin()
+  chat("[HELIOS]: Starting all accounts.")
+  -- do tp stuff, hacky alt stuff
+end)
+
+function welcome()
+  chat("[HELIOS]: Thank you for buying w/ Helios's DHC")
+  wait(5)
+  chat("[HELIOS]: Give us a second, to prepare drop stage.")
+end)
